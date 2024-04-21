5a39958cfb015ca220ecec9202af7ae85a2c4359
마도 <mydodo1120@naver.com>
2024-04-21 23:57:32 +0900

    End

---
+++ Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainButtonClick : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
